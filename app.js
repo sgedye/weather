579c92c87e3f7d74ea4837093abbb0735381cf3a
Shaun Gedye <sgedye88@yahoo.com.au>
2020-04-29 22:44:27 +0800

    Added Skycons

--- app.js
+++ app.js
@@ -19,27 +19,54 @@ document.addEventListener("DOMContentLoaded", () => {
   const lastChecked = document.getElementById("last-checked")
   const feelsLike = document.getElementById("feel-temp")
   const humidity = document.getElementById("humidity")
+  const weatherIcon = document.getElementById("weather-icon")
+
   const proxy = 'https://cors-anywhere.herokuapp.com/'
   const api = 'http://reg.bom.gov.au/fwo/IDW60901/IDW60901.94608.json' // BOM Data for Perth Observations - Individual Stations
+
   fetch(proxy + api)
     .then(response => response.json())
     .then(data => {
-      //console.log(data.observations.data[3])
-      const { refresh_message, state_time_zone } = data.observations.header[0]
-      const {name, apparent_t, air_temp, local_date_time, rel_hum } = data.observations.data[0]
-      //const dataArray = data.observations.data
-      //console.log("Issue time", lastUpdated)
-      console.log(name, apparent_t, air_temp, local_date_time, rel_hum)
+      const { state_time_zone } = data.observations.header[0]
+      const { name, apparent_t, air_temp, local_date_time, rel_hum } = data.observations.data[0]
+      const { wind_spd_kmh, rain_trace, cloud } = data.observations.data[0]
 
       location.innerHTML = `${name}, ${state_time_zone}`
       realTemperature.innerHTML = `${air_temp}°C`
-      feelsLike.innerHTML = `Feels Like: <strong>${apparent_t}°C</strong>`
       let lastReport = local_date_time.replace(/\d+\/0?/, '')
-
+      console.log(lastReport)
       lastChecked.innerHTML = `Accurate as of: <strong>${lastReport}</strong>`
+      feelsLike.innerHTML = `Feels Like: <strong>${apparent_t}°C</strong>`
       humidity.innerHTML = `Humidity: <strong>${rel_hum}%</strong>`
+
+      setWeatherIcon(wind_spd_kmh, rain_trace, cloud, lastReport)
     })
 
+  function setWeatherIcon(wind, rain, cloud, time) {
+    let militaryTime = time.includes('pm') 
+      ? Number(time.replace(/\D/g, '')) + 1200
+      : Number(time.replace(/\D/g, ''))
+    let isDay = (militaryTime >= 0600 && militaryTime < 1800)
+    let iconName
+    console.log(time, militaryTime, isDay)
+    if (Number(rain) >= 0.5) {
+      iconName = 'RAIN'
+    } else if (wind >= 10) {
+      iconName = 'WIND'
+    } else if (cloud === "Cloudy") {
+      iconName = 'CLOUDY'
+    } else if (cloud === "Partly cloudy") {
+      iconName = isDay ? 'PARTLY_CLOUDY_DAY' : 'PARTLY_CLOUDY_NIGHT'
+    } else {
+      iconName = isDay ? 'CLEAR_DAY' : 'CLEAR_NIGHT'
+    }
+
+    const skycons = new Skycons({'color': 'white'})
+    skycons.play()
+    return skycons.set(weatherIcon, Skycons[iconName])
+  }
+
+  
   const convertToFahrenheit = tempInC => tempInC * 9 / 5 + 32
   const convertToCelsius = tempInF => (tempInF - 32) * 5 / 9
 
